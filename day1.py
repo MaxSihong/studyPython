3211b0f44b43b48f789733c21123862c2a014a0b
MaxSihong <maxsihong@163.com>
2025-02-06 23:51:20 +0800

    feat[day1]: print

--- day1.py
+++ day1.py
@@ -10,3 +10,23 @@ print('this is one', 'this is two', 'this is three', sep='|')
 # 设置结束，默认\n换行
 print('setEnd', end='')
 print('frontNotHasEnd')
+
+# -------------------------
+
+bookPrice = 100.00
+eggPrice = 2.00
+testFlow = 2.88888668
+age = 8
+
+print('书价格：%s' % bookPrice, '鸡蛋价格%s' % eggPrice)
+print('书价格：%s，鸡蛋价格%s' % (bookPrice, eggPrice))
+print(f"书价格：{bookPrice}，鸡蛋价格{eggPrice}")
+print('年龄：%d' % age)
+# 默认小数6位，超过则四舍五入
+print('书价格：%f，鸡蛋价格%f' % (bookPrice, eggPrice))
+print('testFlow %f' % testFlow)
+print('testFlow %.2f' % testFlow)
+print('bookPrice %.2f' % bookPrice)
+# 位数8用0补全，超过则原样输出
+no = 8
+print('%08d' % no)
