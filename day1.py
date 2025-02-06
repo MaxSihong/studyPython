92a913645cb5f17c4e772be0bcab4dd60ea95498
chenzhihong_akkj <chenzhihong@outku.com>
2025-02-06 21:41:26 +0800

    first commit

---
+++ day1.py
@@ -0,0 +1,12 @@
+print('Hello maxsihong')
+print('Hello maxsihong2')
+print('Hello maxsihong3')
+print('Hello maxsihong4')
+
+# 输出多句话
+print('this is one', 'this is two', 'this is three')
+# 设置多句话的分割，默认空格
+print('this is one', 'this is two', 'this is three', sep='|')
+# 设置结束，默认\n换行
+print('setEnd', end='')
+print('frontNotHasEnd')
